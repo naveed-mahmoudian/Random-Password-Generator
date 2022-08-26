6a1d7e5e04bb785667d9bdcf201c67ed74e6cc5e
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-08-26 16:01:31 -0700

    Added variables needed to get started

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specialChars = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "/", "^", "|", "{", "}", "~"]
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  var numChars = window.prompt("How many characters in the password?");
+
+  function generatePassword(){
+    
+  }
 
   passwordText.value = password;
 
