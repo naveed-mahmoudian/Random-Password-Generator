512a9989b2ab9d8b82c333a2902a7b801748a05a
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-08-28 16:08:42 -0700

    Added all probablities for password generation

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var specialChars = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "/", "^", "|", "{", "}", "~"]
+var numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specialCharsArr = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "/", "^", "|", "{", "}", "~"]
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
 
   var numChars = window.prompt("How many characters in the password?");
@@ -16,13 +16,47 @@ function writePassword() {
   var includeUppercase = window.confirm("Include uppercase letters? \nOK = YES \nCANCEL = NO");
   var includeNumbers = window.confirm("Include numbers? \nOK = YES \nCANCEL = NO");
   var includeSpecialChars = window.confirm("Include special characters? \nOK = YES \nCANCEL = NO");
- 
-  function generatePassword(){
-    
-  }
+  
+  var password = generatePassword(numChars, includeLowercase, includeUppercase, includeNumbers, includeSpecialChars);
 
-  passwordText.value = password;
+    function generatePassword(chars, lowercase, uppercase, numbers, specialChars){
+      var generatedPass = "";
+      var randomIndex;
 
+      if (lowercase) {
+          for (i=0; i < chars; i++) {
+            randomIndex = Math.floor(Math.random() * lowercaseChars.length);
+            generatedPass += lowercaseChars[randomIndex];
+          }
+      } else if (lowercase && uppercase){
+        
+      } else if (lowercase && numbers) {
+
+      } else if (lowercase && specialChars) {
+
+      } else if (lowercase && uppercase && numbers) {
+
+      } else if (lowercase && uppercase && numbers && specialChars) {
+
+      } else if (uppercase) {
+
+      } else if (uppercase && numbers) {
+
+      } else if (uppercase && specialChars) {
+
+      } else if (uppercase && numbers && specialChars) {
+
+      } else if (numbers) {
+
+      } else if (numbers && specialChars) {
+
+      } else if (specialChars) {
+
+      } else { generatedPass = "Err: Please select at least one paramaeter" }
+      return generatedPass
+    }
+
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
